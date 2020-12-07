b2f85475475c9676ca5c04c927f30dbcce8edadb
KarinaTelles <karinac.telles@gmail.com>
2020-12-07 14:49:11 -0300

    modelo de dados

---
+++ Modelos/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Razor_PagesMovie.Modelos
+{
+    public class Movie
+    {
+    }
+}
